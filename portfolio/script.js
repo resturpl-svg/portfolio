f6b4c9bf0c48c310c105bc8032c0211a6d3f6f09
resturpl-svg <resturpl@gmail.com>
2026-09-10 08:19:37 +0700

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,8 +0,0 @@
-// Inisialisasi Efek Typing menggunakan Typed.js
-const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Web Designer', 'Content Creator'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
